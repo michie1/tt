98527df26b82b45dc2775c923e53f65b340605d6
Michiel Vos <git@fastfox.nl>
2019-06-04 21:06:14 +0200

    relative path for graphql

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import * as uuid from  'uuid/v4';
 import './App.css';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 });
 
 function TimerButton(props) {
