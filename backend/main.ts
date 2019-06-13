8b062e1bf22ceab17f637ad9bc4401aad24848f7
Michiel Vos <git@fastfox.nl>
2019-06-13 13:16:32 +0200

    start postgraphile from main.ts and expose via express with cors enabled

---
+++ backend/main.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import { postgraphile } from 'postgraphile';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
+const app = express();
+app.use(cors());
+
+const connection = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+app.use(postgraphile(connection, 'public', {
+  graphiql: true,
+}));
+
+app.listen(5000);
