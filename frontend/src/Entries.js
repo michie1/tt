8444aefce1bf39dc3845a8d9e6525c1bd1a9a890
Michiel Vos <git@fastfox.nl>
2019-06-15 10:42:37 +0200

    Show entries by date

--- frontend/src/Entries.js
+++ frontend/src/Entries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import format from 'date-fns/format';
 import * as api from './api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
@@ -13,7 +14,8 @@ const tdStyle = {
 };
 
 function Entry(props) {
-  return <tr style={trStyle}>
+  return (
+    <tr style={trStyle}>
         <td>
           {props.entry.text}
         </td>
@@ -24,20 +26,47 @@ function Entry(props) {
           </button>
           <button className="delete is-large" onClick={() => { props.delete(); }}>Delete</button>
         </td>
-      </tr>;
+    </tr>
+  );
 }
 
 function Entries(props) {
-  return <div>
-    <h3 className="title is-3">Entries</h3>
-    <table className="table is-fullwidth">
-      <tbody>
-        {props.entries
-          .sort((a, b) => {
-            return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  const groups = Object.entries(props.entries
+    .reduce((soFar, entry) => {
+      const date = new Date(entry.createdAt);
+      const formatted = format(date, 'YYYY-MM-DD');
+
+      Object.assign(soFar, {
+        [formatted]: [entry, ...(soFar[formatted] || [])],
+      });
+      return soFar;
+    }, {}))
+    .sort(([dateA], [dateB]) => {
+      return new Date(dateB).getTime() - new Date(dateA).getTime();
+    })
+    .map(([date, entries]) => {
+      return {
+        date,
+        entries: entries
+          .sort((entryA, entryB) => {
+            return new Date(entryB.createdAt).getTime() - new Date(entryA.createdAt).getTime();
           })
-          .map((entry) => {
-              return <Entry
+      };
+    });
+
+  return (
+    <div>
+      <h3 className="title is-3">Entries</h3>
+      {groups
+        .map(({date, entries}) => {
+          return (
+            <div key={date}>
+            <h4 className="title is-4">{date}</h4>
+            <table className="table is-fullwidth">
+            <tbody>
+            {entries
+              .map((entry) => {
+                return <Entry
                 key={entry.id}
                 entry={entry}
                 started={props.started}
@@ -56,12 +85,17 @@ function Entries(props) {
                       });
                     });
                 }}
-              />;
-          })
-        }
-        </tbody>
-      </table>
-    </div>;
+                  />;
+              })
+            }
+            </tbody>
+            </table>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
 }
 
 export {
